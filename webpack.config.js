a533aab0e03826c0437515f8f35c35842589ad78
Chathan Driehuys <chathan@knowmetools.com>
2017-11-22 23:46:13 -0500

    Add typescript support.

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,23 @@ const SOURCE_DIR = path.resolve(__dirname, 'src');
 
 
 module.exports = {
-  entry: path.resolve(SOURCE_DIR, 'index.js'),
+  entry: path.resolve(SOURCE_DIR, 'index.ts'),
+  module: {
+    rules: [
+      {
+        include: SOURCE_DIR,
+        loader: 'awesome-typescript-loader',
+        test: /\.tsx?$/,
+      },
+    ],
+  },
   output: {
     filename: 'index.js',
     library: 'KMReactComponents',
     libraryTarget: 'umd',
     path: BUILD_DIR,
-  }
+  },
+  resolve: {
+    extensions: ['ts', 'tsx', 'js'],
+  },
 };
